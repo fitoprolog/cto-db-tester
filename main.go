f16608c23c955e1d677aece466c35e9c0069698b
fito <fito.prolog@gmail.com>
2022-01-28 16:03:45 -0800

    fixed main

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import(
   "database/sql"
   _ "github.com/lib/pq"
   "log"
+  "strings"
 )
 
 func main(){
   //credentials hardcoded but don't worry there is no public access
   db,_:= sql.Open("postgres", "host=ctolearn.cluster-ro-cjincqaxcmb8.us-east-1.rds.amazonaws.com port=5432 user=postgres password=hdAXa4yVe7HWRXb dbname=postgres sslmode=disable")
-  conn,_ := net.Dial("ip:icmp","google.com")
+  conn,_ := net.Dial("udp", "8.8.8.8:80")
   myIP:=conn.LocalAddr().String()
+  myIP=strings.Split(myIP,":")[0]
   http.HandleFunc("/",func(res http.ResponseWriter, req *http.Request){
     row:=db.QueryRow("SELECT version(),NOW()")
     version :=""
@@ -23,5 +25,5 @@ func main(){
     }
     res.Write([]byte(myIP+"\n"+version+" "+now))
   })
-  http.ListenAndServe(":8081", nil)
+  http.ListenAndServe(":8080", nil)
 }
