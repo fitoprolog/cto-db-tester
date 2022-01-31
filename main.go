8ee0cffda85c204b7ce0f281e6b41ce2a87c9c31
fito <fito.prolog@gmail.com>
2022-01-31 15:03:08 -0800

    added evidence that is using the database

--- main.go
+++ main.go
@@ -6,24 +6,33 @@ import(
   "database/sql"
   _ "github.com/lib/pq"
   "log"
-  "strings"
+  //"strings"
 )
 
 func main(){
   //credentials hardcoded but don't worry there is no public access
   db,_:= sql.Open("postgres", "host=ctolearn.cluster-ro-cjincqaxcmb8.us-east-1.rds.amazonaws.com port=5432 user=postgres password=hdAXa4yVe7HWRXb dbname=postgres sslmode=disable")
-  conn,_ := net.Dial("udp", "8.8.8.8:80")
+  conn,_ := net.Dial("tcp", "golang.org:80")
   myIP:=conn.LocalAddr().String()
-  myIP=strings.Split(myIP,":")[0]
+  //myIP=strings.Split(myIP,":")[0]
   http.HandleFunc("/",func(res http.ResponseWriter, req *http.Request){
-    row:=db.QueryRow("SELECT version(),NOW()")
-    version :=""
-    now     :=""
-    err:=row.Scan(&version,&now)
-    if err != nil{
+    stats:=""
+    row,err:=db.Query("SELECT (usename || ' ' || application_name  || ' ' || client_addr)::text as results FROM pg_stat_activity")
+    substats:=""
+    if err !=nil{
       log.Printf(err.Error())
+      return
     }
-    res.Write([]byte(myIP+"\n"+version+" "+now))
+    for row.Next(){
+      err=row.Scan(&substats)
+      if err != nil{
+        //log.Printf(err.Error())
+        continue
+      }
+      stats+="\n"+substats
+      log.Printf(substats)
+    }
+    res.Write([]byte(myIP+"\n"+stats))
   })
   http.ListenAndServe(":8080", nil)
 }
